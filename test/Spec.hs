d45f6e87025d1169a242cd91418f26a580f02e00
gentj <Jamie.Gentry@pega.com>
2017-11-12 13:40:46 -0500

    Simplify test by removing record syntax

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,11 +6,10 @@ main = hspec $ do
   describe "Cube" $ do
     it "new cube" $ do
       startingCube `shouldBe`
-        Cube {
-          f = Face Orange Orange Orange Orange Orange Orange Orange Orange Orange,
-          l = Face Green Green Green Green Green Green Green Green Green,
-          u = Face Yellow Yellow Yellow Yellow Yellow Yellow Yellow Yellow Yellow,
-          d = Face White White White White White White White White White,
-          r = Face Blue Blue Blue Blue Blue Blue Blue Blue Blue,
-          b = Face Red Red Red Red Red Red Red Red Red
-        }
+        Cube
+            (Face Orange Orange Orange Orange Orange Orange Orange Orange Orange)
+            (Face Green Green Green Green Green Green Green Green Green)
+            (Face Yellow Yellow Yellow Yellow Yellow Yellow Yellow Yellow Yellow)
+            (Face White White White White White White White White White)
+            (Face Blue Blue Blue Blue Blue Blue Blue Blue Blue)
+            (Face Red Red Red Red Red Red Red Red Red)
